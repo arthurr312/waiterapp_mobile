667036354b484f56ad45e7dce6a0dcd2bcc9cecd
Arthur Silva <arthur10.silva312@gmail.com>
2023-04-29 10:31:05 -0300

    first commit

---
+++ src/components/Categories/index.tsx
@@ -0,0 +1,26 @@
+import { Text } from "../Text";
+import { Category, Icon } from "./styles";
+import { categories } from "../../mocks/categories";
+import { FlatList } from "react-native";
+
+export function Categories() {
+    return (
+        <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{paddingRight: 24}}
+            data={categories}
+            keyExtractor={(category) => category._id}
+            renderItem={({ item: category }) => (
+                <Category>
+                    <Icon>
+                        <Text>{category.icon}</Text>
+                    </Icon>
+                    <Text size={14} weight="600">
+                        {category.name}
+                    </Text>
+                </Category>
+            )}
+        />
+    );
+}
